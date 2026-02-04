6ba4335bd0dc92fd793c8d95d410cbb6a3dfa373
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-02-04 15:41:16 -0300

    improv

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import turtle
-from secrets import token_urlsafe
 from scoreboard import Scoreboard
-import pandas
 
 
 scoreboard = Scoreboard()
@@ -12,28 +10,15 @@ screen.addshape(image)
 turtle.shape(image)
 
 
-states_data = pandas.read_csv("50_states.csv")
-states_dict = states_data.to_dict()
-states_list = states_data["state"].to_list()
-
-
-# Record the correct guesses in a list
-correct_guesses = []
-
-
 # Use a loop to allow the user to keep guessing
-while len(correct_guesses) < 50:
-    answer_state = screen.textinput(title=f"{len(correct_guesses)}/50 Guess the States", prompt="What's another state's name?")
+while len(scoreboard.correct_guesses) < 50:
+    answer_state = screen.textinput(title=f"{len(scoreboard.correct_guesses)}/50 Guess the States",
+                                    prompt="What's another state's name?")
     titled_answer = answer_state.title()
 
-    if titled_answer not in correct_guesses:
+    if titled_answer not in scoreboard.correct_guesses:
         # Check if the guess is among the 50 states
-        if titled_answer in states_list:
+        if titled_answer in scoreboard.states_list:
             # Write correct guesses onto the map
-            state_pos = states_data[states_data.state == f"{titled_answer}"]
-            state_pos_x = state_pos.x.iloc[0]
-            state_pos_y = state_pos.y.iloc[0]
-            scoreboard.add_state_screen(state_pos_x, state_pos_y, titled_answer)
-            correct_guesses.append(titled_answer)
-
+            scoreboard.add_state_screen(titled_answer)
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import pandas
 
 
 class Scoreboard(Turtle):
@@ -7,7 +8,18 @@ class Scoreboard(Turtle):
         self.color("black")
         self.penup()
         self.hideturtle()
+        self.states_data = pandas.read_csv("50_states.csv")
+        self.states_list = self.states_data["state"].to_list()
+        self.correct_guesses = set()
 
-    def add_state_screen(self, pos_x, pos_y, name_state):
-        self.goto(pos_x, pos_y)
-        self.write(arg=f"{name_state}", move=False, align="center", font=("Courier", 14, "normal"))
+
+    def add_state_screen(self, name_state):
+        state_pos = self.states_data[self.states_data.state == name_state]
+        state_pos_x = state_pos.x.iloc[0]
+        state_pos_y = state_pos.y.iloc[0]
+        self.goto(state_pos_x, state_pos_y)
+        self.write(arg=f"{name_state}",
+                   move=False,
+                   align="center",
+                   font=("Courier", 11, "normal"))
+        self.correct_guesses.append(name_state)
